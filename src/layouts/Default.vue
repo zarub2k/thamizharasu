0f0ea47d8058ee105b01cff8ae643daffd2818ff
Tham <zarub2k@gmail.com>
2020-11-23 16:26:27 +0530

    nav_link is removed

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,9 +7,9 @@
     <div class="bg-gray-500">
       <div class="flex">
         <div class="flex-1"><g-link class="" to="/">Home</g-link></div>
-        <div class="flex-1"><g-link class="nav__link" to="/blog/">Blog</g-link></div>
-        <div class="flex-1"><g-link class="nav__link" to="/about/">About</g-link></div>
-        <div class="flex-1"><g-link class="nav__link" to="/contact/">Contact</g-link></div>
+        <div class="flex-1"><g-link class="" to="/blog/">Blog</g-link></div>
+        <div class="flex-1"><g-link class="" to="/about/">About</g-link></div>
+        <div class="flex-1"><g-link class="" to="/contact/">Contact</g-link></div>
       </div>
     </div>
     <div class="col-span-2 ...">
