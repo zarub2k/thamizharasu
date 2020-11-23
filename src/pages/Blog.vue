8153d382a445994f35d5f33b15c4702ccfe4a002
Tham <zarub2k@gmail.com>
2020-11-23 13:29:16 +0530

    Blog style work started

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,5 +1,13 @@
 <template>
   <Layout>
+    <div class="grid grid-cols-2 gap-4">
+      <div v-for="post in $page.posts.edges" :key="post.id" >
+        <h2 class="text-xl font-medium text-black"><g-link :to="post.node.path" rel="bookmark">{{ post.node.title }}</g-link></h2>
+        <p class="text-xs text-gray-500">Posted on <time :datetime="post.node.date">{{ post.node.date }}</time></p>
+
+        <p>{{ post.node.summary }}</p>
+      </div>
+    </div>
     <div>
       <article v-for="post in $page.posts.edges" :key="post.id" >
         <h2 class="text-xl font-medium text-black"><g-link :to="post.node.path" rel="bookmark">{{ post.node.title }}</g-link></h2>
