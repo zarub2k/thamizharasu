c8742eae2f1e96e836799590db7483275526b631
Tham <zarub2k@gmail.com>
2020-11-23 17:08:43 +0530

    padding clean up is done

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="grid grid-cols-2">
-    <div class="bg-gray-500">
-      <header>
-        <g-link class="text-2xl text-gray-200" to="/">{{ $static.metadata.siteName }}</g-link>
-      </header></div>
-    <div class="flex bg-gray-500 text-gray-200 content-center">
-      <div class="flex-1"><g-link class="" to="/">Home</g-link></div>
-      <div class="flex-1"><g-link class="" to="/blog/">Blog</g-link></div>
-      <div class="flex-1"><g-link class="" to="/about/">About</g-link></div>
-      <div class="flex-1"><g-link class="" to="/contact/">Contact</g-link></div>
+  <!-- <div class="container px-10 bg-gray-100"> -->
+    <div class="grid grid-cols-2">
+      <div class="bg-gray-500 p-3">
+        <header>
+          <g-link class="text-2xl text-gray-200" to="/">{{ $static.metadata.siteName }}</g-link>
+        </header>
+      </div>
+      <div class="flex bg-gray-500 text-gray-200 content-center p-3">
+        <div class="flex-1"><g-link class="" to="/">Home</g-link></div>
+        <div class="flex-1"><g-link class="" to="/blog/">Blog</g-link></div>
+        <div class="flex-1"><g-link class="" to="/about/">About</g-link></div>
+        <div class="flex-1"><g-link class="" to="/contact/">Contact</g-link></div>
+      </div>
+      <div class="col-span-2 p-3">
+        <slot/>
+      </div>
+      <div class="col-span-2 bg-gray-300 p-3">
+        <footer>
+          &copy;2020 Thamizharasu
+        </footer>
+      </div>
     </div>
-    <div class="col-span-2 ...">
-      <slot/>
-    </div>
-    <div class="col-span-2">
-      <footer class="bg-gray-300 h-10 align-middle">
-        &copy;2020 Thamizharasu
-      </footer>
-    </div>
-  </div>
+  <!-- </div> -->
 </template>
 
 <static-query>
