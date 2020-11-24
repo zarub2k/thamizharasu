a35ab3a31ccbd356a27d37836a09c3746dd1f697
Tham <zarub2k@gmail.com>
2020-11-24 20:36:20 +0530

    Style work started

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,7 +1,15 @@
 <template>
   <Layout>
     <div class="grid grid-cols-4 gap-4">
-      <div v-for="post in $page.posts.edges" :key="post.id" >
+      <div v-for="post in $page.posts.edges" :key="post.id">
+        <div class="border-gray-500">
+          <g-image alt="Example image" :src="require(`!!assets-loader!@images/${post.node.thumbnail}`)" width="150" />
+          <p class="text-xs text-gray-400">
+            <time :datetime="post.node.date">{{ post.node.date }}</time>
+          </p>
+        </div>
+      </div>
+      <!-- <div v-for="post in $page.posts.edges" :key="post.id" >
         <g-image alt="Example image" :src="require(`!!assets-loader!@images/${post.node.thumbnail}`)" width="150" />
         <h2 class="text-xl font-medium text-black">
           <g-link :to="post.node.path" rel="bookmark">{{ post.node.title }}</g-link>
@@ -9,7 +17,7 @@
         <p class="text-xs text-gray-500">Posted on <time :datetime="post.node.date">{{ post.node.date }}</time></p>
 
         <p>{{ post.node.summary }}</p>
-      </div>
+      </div> -->
     </div>
   </Layout>
 </template>
