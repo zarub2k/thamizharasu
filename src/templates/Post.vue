257b78267940961dcad1a9ebd378c4568dd67b73
Tham <zarub2k@gmail.com>
2020-11-26 19:44:00 +0530

    Style change is done

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -6,8 +6,8 @@
         <div class="grid grid-cols-4 gap-4">
           <div class="col-start-1 col-end-4">
             <article class="col-start-1 col-end-4 prose max-w-none" v-html="$page.post.content" />
-            <div class="border mt-5">
-              <h3>About Tham</h3>
+            <div class="border mt-5 p-2">
+              <h3 class="text-lg font-semibold text-indigo-400">About Tham</h3>
               <div class="flex">
                 <div class="flex-shrink-0 mr-5">
                   <img class="w-24 h-24 rounded-full my-auto" src="../assets/tham.png" alt="">
@@ -22,21 +22,41 @@
             </div>
           </div>
 
-          <div class="border">
-            <div class="border">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <div class="">
+            <div class="border mb-3">
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
-              </svg>
-              Author box
+              </svg> -->
+              <div class="bg-indigo-500 bg-opacity-75 text-gray-200 p-2">
+                Author
+              </div>
+              <div class="p-2">
+                Author box content
+              </div>
             </div>
-            <div class="border">
-              Newsletter subscription
+            <div class="border mb-3">
+              <div class="bg-indigo-500 bg-opacity-75 text-gray-100 p-2">
+                Newsletter
+              </div>
+              <div class="p-2">
+                Newsletter box content
+              </div>
             </div>
-            <div class="border">
-              Social box
+            <div class="border mb-3">
+              <div class="bg-indigo-500 bg-opacity-75 text-gray-200 p-2">
+                Social
+              </div>
+              <div class="p-2">
+                Social box content
+              </div>
             </div>
-            <div class="border">
-              Further reading
+            <div class="border mb-3">
+              <div class="bg-indigo-500 bg-opacity-75 text-gray-200 p-2">
+                Further reading
+              </div>
+              <div class="p-2">
+                Further reading box content
+              </div>
             </div>
           </div>
         </div>
